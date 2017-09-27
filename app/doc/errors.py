50a79bccf6a4c37505faa29b6f95df3f567f6e0f
Unknown <752481828@qq.com>
2017-09-27 01:27:26 -0400

    update doc

--- app/doc/errors.py
+++ app/doc/errors.py
@@ -1,40 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import jsonify
 from . import doc
 
 
 @doc.app_errorhandler(400)
 def error_request(error):
-    return render_template('error.html', error='error request', status=400)
+    return jsonify({'msg': 'no', 'error': 'error request'}), 400
 
 
 @doc.app_errorhandler(403)
 def forbidden(error):
-    return render_template('error.html', error='forbidden', status=403)
+    return jsonify({'msg': 'no', 'error': 'forbidden'}), 403
 
 
 @doc.app_errorhandler(404)
 def not_found(error):
-    return render_template('404.html', error='not found', status=404)
+    return jsonify({'msg': 'no', 'error': 'not found'}), 404
 
 
 @doc.app_errorhandler(405)
 def error_method(error):
-    return render_template('error.html', error='request method error', status=405)
+    return jsonify({'msg': 'no', 'error': 'request method error'}), 405
 
 
 @doc.app_errorhandler(408)
 def time_out(error):
-    return render_template('error.html', error='time out', status=408)
+    return jsonify({'msg': 'no', 'error': 'time out'}), 408
 
 
 @doc.app_errorhandler(500)
 def internal_error(error):
-    return render_template('error.html', error='server internal error', status=500)
+    return jsonify({'msg': 'no', 'error': 'server internal error'}), 500
 
 
 @doc.app_errorhandler(503)
 def unavailable(error):
-    return render_template('error.html', error='service unavailable', status=503)
+    return jsonify({'msg': 'no', 'error': 'service unavailable'}), 503
